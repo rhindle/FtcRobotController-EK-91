b802c29f1bbb4cd33f34b8d551e37c4347381608
rhindle <knowlton@gmail.com>
2024-03-06 07:52:50 -0500

    (comments to delete later)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/elliot2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/elliot2.java
@@ -4,8 +4,18 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareDevice;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.configuration.DistributorInfo;
+import com.qualcomm.robotcore.hardware.configuration.ExpansionHubMotorControllerPositionParams;
+import com.qualcomm.robotcore.hardware.configuration.ExpansionHubMotorControllerVelocityParams;
+import com.qualcomm.robotcore.hardware.configuration.annotations.DeviceProperties;
+import com.qualcomm.robotcore.hardware.configuration.annotations.MotorType;
+import com.qualcomm.robotcore.hardware.configuration.typecontainers.MotorConfigurationType;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.hardware.motors.RevRoboticsCoreHexMotor;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Rotation;
 
 @TeleOp (name="elliot2", group="Proto")
 //@Disabled
@@ -29,6 +39,12 @@ public class elliot2 extends LinearOpMode {
         tapeMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         tapeMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         //10 inches = 314 encoder clicks
+       // tapeMotor.setMotorType(@MotorType(ticksPerRev = 288, gearing = 36.25, maxRPM = 137, orientation = Rotation.CCW));
+        //tapeMotor.setMotorType(new MotorConfigurationType().setGearing(36.25));
+       // tapeMotor.setMotorType(RevRoboticsCoreHexMotor);
+        //new MotorConfigurationType().setTicksPerRev(288)
+        //288, 36.25, 137, Rotation.CCW
+       // ticksPerRev=288, gearing=36.25, maxRPM=137, orientation=Rotation.CCW)
 
         //waitForStart();
         while (!isStarted()) {
@@ -69,3 +85,13 @@ public class elliot2 extends LinearOpMode {
         }
     }
 }
+
+// https://github.com/FIRST-Tech-Challenge/FtcRobotController/issues/322
+// https://github.com/OpenFTC/Extracted-RC/blob/master/Hardware/src/main/java/com/qualcomm/hardware/motors/RevRoboticsCoreHexMotor.java
+//@MotorType(ticksPerRev=288, gearing=36.25, maxRPM=137, orientation= Rotation.CCW)
+//@ExpansionHubMotorControllerVelocityParams(P=10, I=3, D=0)
+//@ExpansionHubMotorControllerPositionParams(P=10, I=0.05, D=0)
+//@DeviceProperties(xmlTag="RevRoboticsCoreHexMotor", name="@string/rev_core_hex_name", builtIn = true)
+//@DistributorInfo(distributor="@string/rev_distributor", model="REV-41-1300", url="http://www.revrobotics.com/rev-41-1300")
+
+//https://ftc9929.com/2019/12/16/stress-free-ftc-hardware-configurations/
